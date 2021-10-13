8a35f603d90379e8d49aa1da8dea4cd841ed0191
aisha dorsett <aishadorsett23@gmail.com>
2021-10-13 12:42:03 +0100

    updates to database refactoring

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,17 +5,17 @@ describe Bookmark do
     it "displays all bookmarks" do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES (1, 'http://www.makersacademy.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.github.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
       
       bookmarks = Bookmark.display_bookmarks
     
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.github.com")
-      expect(bookmarks).to include("http://www.google.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
+      expect(bookmarks).to include('http://www.makersacademy.com')
+      expect(bookmarks).to include('http://www.github.com')
+      expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.destroyallsoftware.com')
     end
   end
 end
--- spec/features/viewing_bookmark_spec.rb
+++ spec/features/viewing_bookmark_spec.rb
@@ -1,20 +1,22 @@
 require 'pg'
+
 # feature "Bookmark" do
 #   scenario "view bookmark index page" do
 #     visit ("/")
 #     expect(page).to have_content "Bookmark Manager"
 #   end
+# end
 
 feature "Viewing bookmarks" do
   scenario "can view the bookmarks" do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.github.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(4, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.github.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
 
-    visit("/bookmarks")
+    visit('/bookmarks')
 
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.github.com"
